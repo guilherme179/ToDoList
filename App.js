a44ff1fe210b5cc5c4c6074a01a545922377c279
Matheus Felipe de Oliveira Ferreira <82980047+Matheus-Felipe-C@users.noreply.github.com>
2022-06-21 16:08:16 -0300

    Conexão com Firebase - Conserto de pastas

--- App.js
+++ App.js
@@ -1,81 +1,125 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native';
-import {AntDesign} from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  ActivityIndicator,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import cores from './Cores';
 import dadosTemp from './dadosTemp';
 import TodoList from './componentes/TodoList';
 import AddListModal from './componentes/AddListModal';
-import {initializeApp} from 'firebase/app';
-import '@firebase/firestore';
+import Fire from './Fire';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBX-zOsLCHpTmsgay6hfMNnwlhGQAHMcZ4",
-    authDomain: "todolist-3bdf0.firebaseapp.com",
-    projectId: "todolist-3bdf0",
-    storageBucket: "todolist-3bdf0.appspot.com",
-    messagingSenderId: "237579960933",
-    appId: "1:237579960933:web:b3ae03d9b29ef729f6fc8c"
-}
-
-const app = initializeApp(firebaseConfig);
-
-export default class App extends React.Component{
+export default class App extends React.Component {
   state = {
-    addTodoVisivel : false,
-    lists: dadosTemp
+    addTodoVisivel: false,
+    lists: [],
+    user: {},
+    loading: true,
+  };
+
+  componentDidMount() {
+    firebase = new Fire((error, user) => {
+      if (error) {
+        return alert('Uh oh, algo de errado não está certo');
+      }
+
+      firebase.getLists((lists) => {
+        this.setState({ lists, user }, () => {
+          this.setState({ loading: false });
+        });
+      });
+
+      this.setState({ user });
+    });
+  }
+
+  componentWillUnmount() {
+    firebase.detach();
   }
 
   toggleAddTodoModal() {
-    this.setState({addTodoVisivel: !this.state.addTodoVisivel})
+    this.setState({ addTodoVisivel: !this.state.addTodoVisivel });
   }
 
-  renderList = list =>{
-    return <TodoList list={list} updateList={this.updateList} />
-  }
-
-  addList = list => {
-    this.setState({lists: [...this.state.lists, { ...list, id: this.state.lists.length + 1, todos: [] }] });
-  }
-
-  updateList = list => {
-    this.setState({
-      lists: this.state.lists.map(item => {
-        return item.id === list.id ? list : item
-      })
-    })
+  renderList = (list) => {
+    return <TodoList list={list} updateList={this.updateList} />;
   };
 
-  render(){
+  addList = (list) => {
+    this.setState({
+      lists: [
+        ...this.state.lists,
+        { ...list, id: this.state.lists.length + 1, todos: [] },
+      ],
+    });
+  };
+
+  updateList = (list) => {
+    this.setState({
+      lists: this.state.lists.map((item) => {
+        return item.id === list.id ? list : item;
+      }),
+    });
+  };
+
+  render() {
+    if (this.state.loading) {
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color={cores.azul} />
+        </View>
+      )
+    }
+
     return (
       <View style={styles.container}>
-        <Modal animationType='slide' visible={this.state.addTodoVisivel} onRequestClose={() => this.toggleAddTodoModal()}>
-          <AddListModal closeModal={() => this.toggleAddTodoModal()} addList={this.addList}/>
+        <Modal
+          animationType="slide"
+          visible={this.state.addTodoVisivel}
+          onRequestClose={() => this.toggleAddTodoModal()}>
+          <AddListModal
+            closeModal={() => this.toggleAddTodoModal()}
+            addList={this.addList}
+          />
         </Modal>
-        <View style={{flexDirection: "row"}}>
+        <View>
+          <Text>User:{this.state.user.uid}</Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
           <View style={styles.divisor} />
           <Text style={styles.titulo}>
-            ToDo <Text style={{fontWeight: "300", color: cores.azul}}>Lists</Text>
+            ToDo{' '}
+            <Text style={{ fontWeight: '300', color: cores.azul }}>Lists</Text>
           </Text>
           <View style={styles.divisor} />
         </View>
 
-        <View style={{marginVertical: 48}}>
-          <TouchableOpacity style={styles.addLista} onPress={() => this.toggleAddTodoModal()}>
-            <AntDesign name='plus' size={16} color={cores.azul}/>
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity
+            style={styles.addLista}
+            onPress={() => this.toggleAddTodoModal()}>
+            <AntDesign name="plus" size={16} color={cores.azul} />
           </TouchableOpacity>
 
           <Text style={styles.add}>Add Lista</Text>
         </View>
 
-        <View style={{height: 275,  paddingHorizontal: 18, alignSelf: 'center'}}>
-          <FlatList 
-            data={this.state.lists} 
-            keyExtractor={item => item.name} 
-            horizontal={true} 
-            showsHorizontalScrollIndicator={false} 
-            renderItem={({item}) => this.renderList(item)}
-            keyboardShouldPersistTaps = "always"
-            />
+        <View
+          style={{ height: 275, paddingHorizontal: 18, alignSelf: 'center' }}>
+          <FlatList
+            data={this.state.lists}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item)}
+            keyboardShouldPersistTaps="always"
+          />
         </View>
       </View>
     );
@@ -92,13 +136,13 @@ const styles = StyleSheet.create({
     backgroundColor: cores.azulClaro,
     height: 1,
     flex: 1,
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   titulo: {
     fontSize: 38,
-    fontWeight: "800",
+    fontWeight: '800',
     color: cores.preto,
-    paddingHorizontal: 64
+    paddingHorizontal: 64,
   },
   addLista: {
     borderWidth: 2,
@@ -107,13 +151,13 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 16,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   add: {
     color: cores.preto,
-    fontWeight: "600",
+    fontWeight: '600',
     fontSize: 14,
     marginTop: 8,
     alignSelf: 'center',
-  }
+  },
 });
